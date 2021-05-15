2c588f12d5014796fe7c41e207813847413e978b
edenborrowman <edenborrowman@gmail.com>
2021-05-15 17:58:25 -0400

    notes week 4

--- form_practice/script.js
+++ form_practice/script.js
@@ -1,28 +1,30 @@
-const client_name = document.getElementById('name');
-const password = document.getElementById('password');
-const form = document.getElementById('form');
-const errorElement = document.getElementById('error');
+const client_name = document.querySelector('#name');
+const password = document.querySelector('#password');
+const verify = document.querySelector('#verify');
+const form = document.querySelector('#form');
+const errorElement = document.querySelector('#error');
+
 
 form.addEventListener('submit', (e) => {
     let messages = []
-    if (client_name.value === '' || client_name.value == null){
-        messages.push('Name is required')
+    if (client_name.value === '' || client_name.value == null) {
+        messages.push('Name is required');
     }
 
-    if (password.value.length <= 6) {
-        messages.push('Passowrd must be longer than 6 characters.')
-    }
-
-    if (password.value.length >= 20) {
-        messages.push('Passowrd must be less than 20 characters.')
+    if ((password.value.length <= 6) || (password.value.length >= 20)) {
+        messages.push('Password must be between 6 and 20 characters.');
     }
 
     if (password.value === 'password') {
-        messages.push('Password cannot be password.')
+        messages.push('Password cannot be password.');
     }
 
-    if (messages.length > 0) {    
-    errorElement.innerText = messages.join(', ')
-    e.preventDefault()
+    if (password.value !== verify.value) {
+         messages.push('Passwords do not match');
     }
+
+    if (messages.length > 0) {
+        errorElement.innerText = messages.join(', ');
+        e.preventDefault();
+    }    
 })
