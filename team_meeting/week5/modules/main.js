8fa84c0e32badabe8a1a0863b88006043c36f05c
edenborrowman <edenborrowman@gmail.com>
2021-05-21 18:45:19 -0400

    team project week 5

--- team_meeting/week5/modules/main.js
+++ team_meeting/week5/modules/main.js
@@ -1,53 +1,40 @@
 import Hikes from './script.js';
 //create an array of hikes
-const hikeList = [
-    {
-      name: "Bechler Falls",
-      imgSrc: "falls.jpg",
-      imgAlt: "Image of Bechler Falls",
-      distance: "3 miles",
-      difficulty: "Easy",
-      description:
-        "Beautiful short hike along the Bechler river to Bechler Falls",
-      directions:
-        "Take Highway 20 north to Ashton. Turn right into the town and continue through. Follow that road for a few miles then turn left again onto the Cave Falls road.Drive to the end of the Cave Falls road. There is a parking area at the trailhead.",
-      id: "belcher"
+const hikeList = [{
+        name: "Bechler Falls",
+        imgSrc: "falls.jpg",
+        imgAlt: "Image of Bechler Falls",
+        distance: "3 miles",
+        difficulty: "Easy",
+        description: "Beautiful short hike along the Bechler river to Bechler Falls",
+        directions: "Take Highway 20 north to Ashton. Turn right into the town and continue through. Follow that road for a few miles then turn left again onto the Cave Falls road.Drive to the end of the Cave Falls road. There is a parking area at the trailhead.",
+        id: "belcher"
     },
     {
-      name: "Teton Canyon",
-      imgSrc: "falls.jpg",
-      imgAlt: "Image of Bechler Falls",
-      distance: "3 miles",
-      difficulty: "Easy",
-      description: "Beautiful short (or long) hike through Teton Canyon.",
-      directions:
-        "Take Highway 33 East to Driggs. Turn left onto Teton Canyon Road. Follow that road for a few miles then turn right onto Staline Raod for a short distance, then left onto Alta Road. Veer right after Alta back onto Teton Canyon Road. There is a parking area at the trailhead.",
+        name: "Teton Canyon",
+        imgSrc: "falls.jpg",
+        imgAlt: "Image of Bechler Falls",
+        distance: "3 miles",
+        difficulty: "Easy",
+        description: "Beautiful short (or long) hike through Teton Canyon.",
+        directions: "Take Highway 33 East to Driggs. Turn left onto Teton Canyon Road. Follow that road for a few miles then turn right onto Staline Raod for a short distance, then left onto Alta Road. Veer right after Alta back onto Teton Canyon Road. There is a parking area at the trailhead.",
         id: "teton"
     },
     {
-      name: "Denanda Falls",
-      imgSrc: "falls.jpg",
-      imgAlt: "Image of Bechler Falls",
-      distance: "7 miles",
-      difficulty: "Moderate",
-      description:
-        "Beautiful hike through Bechler meadows river to Denanda Falls",
-      directions:
-        "Take Highway 20 north to Ashton. Turn right into the town and continue through. Follow that road for a few miles then turn left again onto the Cave Falls road. Drive to until you see the sign for Bechler Meadows on the left. Turn there. There is a parking area at the trailhead.",
-      id: "deanda"
+        name: "Denanda Falls",
+        imgSrc: "falls.jpg",
+        imgAlt: "Image of Bechler Falls",
+        distance: "7 miles",
+        difficulty: "Moderate",
+        description: "Beautiful hike through Bechler meadows river to Denanda Falls",
+        directions: "Take Highway 20 north to Ashton. Turn right into the town and continue through. Follow that road for a few miles then turn left again onto the Cave Falls road. Drive to until you see the sign for Bechler Meadows on the left. Turn there. There is a parking area at the trailhead.",
+        id: "deanda"
     }
-  ];
-
-  const hikeListElement = document.getElementById("hikes");
-
-  const fullDetails = document.getElementById("mouseover");
-
-  const hikeObject = new Hikes();
-
- //on load grab the array and insert it into the page
- window.addEventListener("load", () => {
+];
+const hikeListElement = document.getElementById("hikes");
+const fullDetails = document.getElementById("click");
+const hikeObject = new Hikes();
+//on load grab the array and insert it into the page
+window.addEventListener("load", () => {
     hikeObject.showHikeList(hikeList, hikeListElement);
-  });
-
-
-
+});
--- team_meeting/week5/modules/script.js
+++ team_meeting/week5/modules/script.js
@@ -14,7 +14,7 @@ export default class Hikes {
   }
   renderOneHike(hike) {
     const item = document.createElement("li");
-
+    item.setAttribute('id', hike.id)
     item.innerHTML = ` <h2>${hike.name}</h2>
           <div class="image"><img src="${this.imgBasePath}${hike.imgSrc}" alt="${hike.imgAlt}" id=${hike.id}></div>
           <div>
@@ -27,48 +27,50 @@ export default class Hikes {
                       <p>${hike.difficulty}</p>
                   </div>
           </div>`;
-
     return item;
   }
   setImageEventListener(hikes) {
     hikes.forEach((hike) => {
-      document.getElementById(hike.id).addEventListener("mouseenter", () => {
-        console.log(hike.distance);
-        document.getElementById("mouseover").classList.remove("nodisplay");
+      //document.getElementById("mybtn").addEventListener("click", () {
+      document.getElementById(hike.id).addEventListener("click", event => {
+        event.preventDefault();
+        const fullDetails = document.getElementById(hike.id);
+        fullDetails.innerHTML = ` <h2>${hike.name}</h2>
+                  <div>
+                      <h3>Distance</h3>
+                      <p>${hike.distance}</p>
+                  </div>
+                  <div>
+                      <h3>Difficulty</h3>
+                      <p>${hike.difficulty}</p>
+                  </div>
+                  <h3>Description</h3>
+              <p>${hike.description}</p><br>
+           <h3>Directions</h3>
+               <p>${hike.directions}</p><br> 
+               <button id="mybtn${hike.id}">Go Back</button>
+              `;
+        document.getElementById("mybtn" + hike.id).addEventListener("click", event => {
+          event.stopPropagation();
+          const backDetails = document.getElementById(hike.id);
+          backDetails.innerHTML = ` <h2>${hike.name}</h2>
+            <div class="image"><img src="${this.imgBasePath}${hike.imgSrc}" alt="${hike.imgAlt}" id=${hike.id}></div>
+            <div>
+                    <div>
+                        <h3>Distance</h3>
+                        <p>${hike.distance}</p>
+                    </div>
+                    <div>
+                        <h3>Difficulty</h3>
+                        <p>${hike.difficulty}</p>
+                    </div>
+            </div>`;
+        });
       });
     });
   }
-  //CHANGE CLASS WHEN MOUSE IS REMOVED
-  // setImageEventListener(hikes) {
-  //   hikes.forEach(hike => {
-  //     document.getElementById(hike.id).addEventListener(
-  //       'mouseleave', () => {
-  //         console.log("peaches for me");
-  //         document.getElementById('mouseover').classList.add("nodisplay");
-  //       }
-  //     )
-  //   });
-  // }
-
-  // renderHikeFullDetails(hike, fullDetails) {
-  //   // const fullDetails = document.getElementById("mouseover");
-
-  //   fullDetails.innerHTML = ` <h3>Difficulty</h3>
-  //               <p>${hike.name}</p><br>
-  //           <h3>Distance</h3>
-  //               <p>${hike.distance}</p><br>
-  //           <h3>Difficulty</h3>
-  //               <p>${hike.difficulty}</p><br>
-  //           <h3>Description</h3>
-  //               <p>${hike.description}</p><br>
-  //           <h3>Directions</h3>
-  //               <p>${hike.directions}</p><br> `;
-  //   return fullDetails;
-  // }
-
   renderFullDetails(hike) {
-    const fullDetails = document.getElementById("mouseover");
-
+    const fullDetails = document.getElementById("click");
     fullDetails.innerHTML = ` <h2>${hike.name}</h2>
           <div class="image"><img src="${this.imgBasePath}${hike.imgSrc}" alt="${hike.imgAlt}" id=${hike.id}></div>
           <div>
@@ -81,7 +83,6 @@ export default class Hikes {
                       <p>${hike.difficulty}</p>
                   </div>
           </div>`;
-
-    return fullDetails;
+    fullDetails.classList.remove("nodisplay");
   }
-}
+};
