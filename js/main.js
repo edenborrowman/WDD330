65d3da5cc162bb33e5bb608bc67236f056c6e349
edenborrowman <edenborrowman@gmail.com>
2021-06-07 22:28:48 -0400

    Team Meeting Week 8

--- js/main.js
+++ js/main.js
@@ -1,100 +1,105 @@
-// let links = [
-//     {
-//         label: "Week 1",
-//         notes: "Notes and Examples",
-//         url: "https://edenborrowman.github.io/WDD330/wdd330/assignment_index/notes1.html",
-//         group: "no group this week",
-//         assignment: "no assignment this week"
-//     },
+let links = [
+    {
+        label: "Week 1",
+        notes: "Notes and Examples",
+        url: "https://edenborrowman.github.io/WDD330/wdd330/assignment_index/notes1.html",
+        group: "no group this week",
+        assignment: "no assignment this week"
+    },
 
-//     {
-//         label: "Week 2",
-//         notes: "Notes and Examples",
-//         url: "https://edenborrowman.github.io/WDD330/wdd330/assignment_index/notes2.html",
-//         group: "Weekly Group Assignment",
-//         teamUrl: "https://edenborrowman.github.io/WDD330/wdd330/teamMeeting/week2.html",
-//         assignment: "no assignment this week"
+    {
+        label: "Week 2",
+        notes: "Notes and Examples",
+        url: "https://edenborrowman.github.io/WDD330/wdd330/assignment_index/notes2.html",
+        group: "Weekly Group Assignment",
+        teamUrl: "https://edenborrowman.github.io/WDD330/wdd330/teamMeeting/week2.html",
+        assignment: "no assignment this week"
 
-//     },
-//     {
-//         label: "Week 3",
-//         notes: "Notes and Examples",
-//         url: "https://edenborrowman.github.io/WDD330/wdd330/assignment_index/notes3.html",
-//         group: "Weekly Group Assignment",
-//         teamUrl: "https://edenborrowman.github.io/WDD330/wdd330/teamMeeting/week2.html",
-//         assignment: "no assignment this week"
+    },
+    {
+        label: "Week 3",
+        notes: "Notes and Examples",
+        url: "https://edenborrowman.github.io/WDD330/wdd330/assignment_index/notes3.html",
+        group: "Weekly Group Assignment",
+        teamUrl: "https://edenborrowman.github.io/WDD330/wdd330/teamMeeting/week2.html",
+        assignment: "no assignment this week"
 
-//     },
+    },
 
-//     {
-//         label: "Week 4",
-//         notes: "Notes and Examples",
-//         url: "notes4.html",
-//         group: "Weekly Group Assignment",
-//         teamUrl: "team_meeting_week4",
-//         assignment: "Form Validation Exercise",
-//         assignmentUrl: "form_practice"
-//     },
+    {
+        label: "Week 4",
+        notes: "Notes and Examples",
+        url: "notes4.html",
+        group: "Weekly Group Assignment",
+        teamUrl: "team_meeting_week4",
+        assignment: "Form Validation Exercise",
+        assignmentUrl: "form_practice"
+    },
 
-//     {
-//         label: "Week 5: May 16 - May 22",
-//         notes: "Notes and Examples",
-//         url: "notes5.html",
-//         group: "Weekly Group Assignment",
-//         teamUrl: "team_meeting/week5/",
-//         assignment: "no assignment this week"
-//     },
+    {
+        label: "Week 5: May 16 - May 22",
+        notes: "Notes and Examples",
+        url: "notes5.html",
+        group: "Weekly Group Assignment",
+        teamUrl: "team_meeting/week5/",
+        assignment: "no assignment this week"
+    },
 
-//     {
-//         label: "Week 6: May 23 - May 29",
-//         notes: "no notes this week",
-//         group: "no group this week",
-//         assignment: "Midterm Productivity App",
-//         assignmentUrl: "to_do"
-//     },
-//     {
-//         label: "Week 7: May 30 - June 5",
-//         notes: "Notes and Examples",
-//         group: "Weekly Group Assignment",
-//         teamURL: "team_meeting/hike_y/",
-//         assignment: "CSS battle",
-//         assignmentUrl: "https://cssbattle.dev/play/21"        
-//     },
-
-    
+    {
+        label: "Week 6: May 23 - May 29",
+        notes: "no notes this week",
+        group: "no group this week",
+        assignment: "Midterm Productivity App",
+        assignmentUrl: "to_do"
+    },
+    {
+        label: "Week 7: May 30 - June 5",
+        notes: "Notes and Examples",
+        group: "Weekly Group Assignment",
+        teamURL: "team_meeting/hike_y/",
+        assignment: "CSS battle",
+        assignmentUrl: "https://cssbattle.dev/play/21"        
+    },
 
     
-// ]
-
-
-// var i;
-
-// for(i = 0; i<links.length; i++){
-
-//     let card = document.createElement('div');
-//     let num_list = document.createElement('ol');
-//     let work_id = document.createElement('h4');
-//     let notes = document.createElement('li');
-//     let team = document.createElement('li');
-//     let assignment = document.createElement('li');
 
     
-//     work_id.textContent = links[i].label;
-//     notes.textContent= links[i].notes;
-//     notes.setAttribute('href', links[i].url);
-//     team.textContent= links[i].group;
-//     team.setAttribute('href', links[i].teamUrl);    
-//     assignment.textContent= links[i].assignment;
-//     assignment.setAttribute('href', links[i].assignmentUrl);
+]
 
-//     card.appendChild(work_id);
-//     card.appendChild(num_list);
-//     card.appendChild(notes);
-//     card.appendChild(team);
-//     card.appendChild(assignment);
+
+var i;
+
+for(i = 0; i<links.length; i++){
+
+    let card = document.createElement('div');
+    let assignment_group = document.createElement('ol');
+    let work_id = document.createElement('ul');
+    let notes = document.createElement('li');
+    let team = document.createElement('li');
+    let assignment = document.createElement('li');
+
+   
+    assignment_group.textContent = links[i].label;    
+    notes.textContent= links[i].notes;    
+    team.textContent= links[i].group;       
+    assignment.textContent= links[i].assignment;
+    card.setAttribute('class', 'cards');
+    work_id.setAttribute('class', 'new_entry');
+    assignment_group.setAttribute('ol', 'assignment_group')
+    notes.setAttribute('href', links[i].url);
+    team.setAttribute('href', links[i].teamUrl); 
+    assignment.setAttribute('href', links[i].assignmentUrl);
+
+    card.appendChild(assignment_group);
+    card.appendChild(work_id);
+    card.appendChild(notes);
+    card.appendChild(team);
+    card.appendChild(assignment);
    
 
-//     document.querySelector('div.cards').appendChild(card);
+    document.querySelector('div.cards').appendChild(card);
+   
+    
     
     
  
@@ -142,4 +147,4 @@ document.getElementById("currentdate").textContent = fulldate;
 document.getElementById("currentyear").textContent = d.getFullYear();
 
 let oLastModif = new Date(document.lastModified);
-document.getElementById("lastModified").textContent = oLastModif;
+document.getElementById("lastModified").textContent = oLastModif};
---
+++ team_meeting/week8/script.js
@@ -0,0 +1,99 @@
+const requestURL = "https://swapi.dev/api/people"
+
+function fetch_info(url){
+fetch(url)
+    .then(response => response.json())
+    .then(json => {
+        console.table(json)
+
+const starwars = json['results'];
+// return starwars
+console.table(starwars);
+
+let num_pages = Math.ceil(json.count / 10);
+console.log(num_pages);
+
+    
+document.querySelector('div.tradingCard').innerHTML="";
+document.querySelector('div.wrapper').innerHTML="";
+
+let previous = document.createElement('button');
+previous.setAttribute('id', 'back');
+previous.innerText= 'Back';
+
+document.querySelector('div.wrapper').appendChild(previous);
+
+for(let i=1; i <= num_pages; i++){
+    // console.log('button' + i);
+    // document.createElement('a')
+    let button = document.createElement('button');
+    button.innerText = i;
+    document.querySelector('div.wrapper').appendChild(button);
+    button.addEventListener("click", () => fetch_info('https://swapi.dev/api/people/?page=' + i));
+}
+
+let next = document.createElement('button');
+next.setAttribute('id', 'next');
+next.innerText = 'Next';
+
+
+next.addEventListener("click", () => fetch_info(json.next));
+previous.addEventListener("click", () => fetch_info(json.previous));
+document.querySelector('div.wrapper').appendChild(next);
+
+
+
+
+for(let i=0; i < starwars.length; i++) {
+    let card = document.createElement('section');
+    let info_div = document.createElement('div');
+    let h2 = document.createElement('h2');
+    let height = document.createElement('p');
+    let mass = document.createElement('p');
+    let hair_color = document.createElement('p');
+    let skin_color = document.createElement('p');
+    let eye_color = document.createElement('p');
+    let birth_year = document.createElement('p');
+    let gender = document.createElement('p');
+    let homeworld = document.createElement('a');
+    
+
+    h2.textContent = starwars[i].name;
+    info_div.setAttribute('class', 'hide');
+    height.textContent = 'Height:' + ' ' + starwars[i].height;
+    mass.textContent = 'Mass:' + ' ' + starwars[i].mass;
+    hair_color.textContent = 'Hair Color:' + ' ' + starwars[i].hair_color;
+    skin_color.textContent = 'Skin Color:' + ' ' + starwars[i].skin_color;
+    eye_color.textContent = 'Eye Color:' + ' ' + starwars[i].eye_color;
+    birth_year.textContent = 'Birth Year:' + ' ' + starwars[i].birth_year;
+    gender.textContent = 'Gender:' + ' ' + starwars[i].gender;
+    homeworld.textcontent = 'Home World' + ' ' + starwars[i].homeworld;
+    homeworld.setAttribute('src', starwars[i].homeworld);
+
+    card.appendChild(h2);
+    card.appendChild(info_div);
+    info_div.appendChild(height);
+    info_div.appendChild(mass);
+    info_div.appendChild(hair_color);
+    info_div.appendChild(skin_color);
+    info_div.appendChild(eye_color);
+    info_div.appendChild(birth_year);
+    info_div.appendChild(gender);
+    info_div.appendChild(homeworld);
+
+    card.addEventListener("click", () => toggleHide(info_div));
+
+    document.querySelector('div.tradingCard').appendChild(card);
+}
+
+});}
+
+fetch_info(requestURL);
+ 
+function toggleMenu() {
+    document.getElementById("primaryNav").classList.toggle("hide");
+  };
+
+function toggleHide(info_div){
+    info_div.classList.toggle('hide');
+}
