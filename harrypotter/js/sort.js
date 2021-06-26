be900b8b77c3555183490aa9438b8599ac42cb17
edenborrowman <edenborrowman@gmail.com>
2021-06-26 00:26:33 -0400

    continued sorting hat css and js

--- harrypotter/js/sort.js
+++ harrypotter/js/sort.js
@@ -5,10 +5,22 @@ let houses = [
 var randomNum = Math.floor(Math.random() * houses.length);
 console.log(randomNum);
 
-var assigned_house = houses[randomNum];
-console.log(assigned_house);
-var dressed = "images/" + houses[randomNum].toLowerCase() + "_student.svg";
-console.log(dressed);
+//add event listener for button click
+document.addEventListener("click", (event) => {
+    var assigned_house = houses[randomNum];
+    console.log(assigned_house);
+    var dressed = "images/" + houses[randomNum].toLowerCase() + "_student.svg";
+    console.log(dressed);
 
-document.querySelector(".announcement").textContent = assigned_house + "!";
-document.querySelector(".student").src = dressed;
+    document.querySelector(".announcement").textContent = assigned_house + "!";
+    document.querySelector(".student").src = dressed;
+    toggleHide(hat);
+    
+});
+
+// function to toggle hat
+
+hat = document.querySelector("#hat")
+function toggleHide(hat) {
+    hat.classList.toggle('hide');
+}
