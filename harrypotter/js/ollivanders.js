3d8980077ec156934ca46675267990d84ede9028
edenborrowman <edenborrowman@gmail.com>
2021-07-01 00:49:07 -0400

    wand shop enhancemnets

---
+++ harrypotter/js/ollivanders.js
@@ -0,0 +1,51 @@
+let wands = [
+    {
+        wood: 'Holly',
+        length: '11.5 inches',
+        core: 'Phoenix Feather',
+        character: 'Springy',
+        url: '/images.wand1.svg'
+    },
+    {
+        wood: 'Yew',
+        length: '13 inches',
+        core: 'Unicorn Hair',
+        character: 'Responsive',
+        url: '/images.wand2.svg',
+    },
+    {
+        wood: 'Ash',
+        length: '10.5 inches',
+        core: 'Dragon Heartstring',
+        character: 'Sensitive',
+        url: '/images.wand3.svg',
+    },
+    {
+        wood: 'Willow',
+        length: '12 inches',
+        core: 'Veela Hair',
+        character: 'Tempermental',
+        url: '/images.wand4.svg',
+    },
+    {
+        wood: 'Cherry',
+        length: '14 inches',
+        core: 'Unicorn Hair',
+        character: 'Powerful',
+        url: '/images.wand5.svg',
+    },
+    {
+        wood: 'Cypress',
+        length: '10 inches',
+        core: 'Dragon Heartstring',
+        character: 'Bendy',
+        url: '/images.wand6.svg',
+    },
+
+]
+console.table(wands);
+
+document.documentElement.style.cursor = "url(wands[0].url)";
+
+// wand application
+
