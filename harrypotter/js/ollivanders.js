4d7d61628461f5b91a3bd9aa1980d6b7dfbc6642
edenborrowman <edenborrowman@gmail.com>
2021-07-01 21:30:24 -0400

    hogwarts

--- harrypotter/js/ollivanders.js
+++ harrypotter/js/ollivanders.js
@@ -4,48 +4,52 @@ let wands = [
         length: '11.5 inches',
         core: 'Phoenix Feather',
         character: 'Springy',
-        url: '/images.wand1.svg'
+        url: 'images/wand1.svg'
     },
     {
         wood: 'Yew',
         length: '13 inches',
         core: 'Unicorn Hair',
         character: 'Responsive',
-        url: '/images.wand2.svg',
+        url: 'images/wand2.svg',
     },
     {
         wood: 'Ash',
         length: '10.5 inches',
         core: 'Dragon Heartstring',
         character: 'Sensitive',
-        url: '/images.wand3.svg',
+        url: 'images/wand3.svg',
     },
     {
         wood: 'Willow',
         length: '12 inches',
         core: 'Veela Hair',
         character: 'Tempermental',
-        url: '/images.wand4.svg',
+        url: 'images/wand4.svg',
     },
     {
         wood: 'Cherry',
         length: '14 inches',
         core: 'Unicorn Hair',
         character: 'Powerful',
-        url: '/images.wand5.svg',
+        url: 'images/wand5.svg',
     },
     {
         wood: 'Cypress',
-        length: '10 inches',
+        length: '10.25 inches',
         core: 'Dragon Heartstring',
         character: 'Bendy',
-        url: '/images.wand6.svg',
+        url: 'images/wand6.svg',
     },
 
 ]
 console.table(wands);
 
-document.documentElement.style.cursor = "url(wands[0].url)";
+let randomWand = Math.floor(Math.random() * wands.length);
+    console.log(randomWand);
 
-// wand application
+document.getElementById("yourWand").src = wands[randomWand].url;
+
+
+
 
